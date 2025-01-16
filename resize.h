0e36e658b757a1c976426856b42f05199665e887
わおん <me@waon.me>
2025-01-16 14:01:20 +0900

    Update resize.h

--- resize.h
+++ resize.h
@@ -1,13 +1,28 @@
 #ifndef RESIZE_H
 #define RESIZE_H
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 int init_resize(int memory_limit);
-int resize(char* input_filename, char* output_filename, int targetWidth, int targetHeight);
+
+/**
+ * advanced_resize:
+ *  - input_filename: 入力ファイルパス
+ *  - output_filename: 出力ファイルパス
+ *  - targetWidth, targetHeight: リサイズ後の幅・高さ
+ *  - quality: 画像の品質(0〜100程度)
+ *  - format: "JPEG", "WEBP", "PNG", "GIF" 等 (大文字推奨)
+ */
+int advanced_resize(
+    char* input_filename,
+    char* output_filename,
+    int targetWidth,
+    int targetHeight,
+    int quality,
+    const char* format
+);
 
 #ifdef __cplusplus
 }
